bf70c41d54b173d5cc9903fb29c0c3f5be1eade7
pcbsytem <pcbsytem@hotmail.com>
2020-06-18 16:58:13 -0300

    Changes in routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
+
+app.use(express.json());
 
 /** 
  * Tipos parâmetros
@@ -12,32 +15,65 @@ const app = express();
  * Body - usado na criação de item
 **/
 
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+  const { title } = request.query;
+
+  const result = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return response.json(result);
 });
 
 app.post('/projects', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 4',
-  ]);
+  const { title, owner } = request.body;
+
+  const project = {
+    id: uuid(),
+    title,
+    owner
+  };
+
+  projects.push(project);
+
+  return response.json(projects);
 });
 
 app.put('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 1'
-  ]);
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: 'Project not found.' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 })
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 1'
-  ]);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: 'Project not found.' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
